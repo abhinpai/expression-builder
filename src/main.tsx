6c1a6ee37e5f27d5e06f5ba39a3ab8483fd44ea3
Abhin Pai <abhinpai96@gmail.com>
2023-07-18 00:46:20 -0400

    Added the tailwind and scss module configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './styles/index.scss'
+import styles from './main.module.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div className={styles.sample}>Hello world!</div>
+  </React.StrictMode>
 )
